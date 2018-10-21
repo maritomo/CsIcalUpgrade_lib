81f59b305b30c0c546d6d26831a0c5c8d22fbc15
maritomo <mari@champ.hep.sci.osaka-u.ac.jp>
2018-10-22 04:58:07 +0900

    linker.sh modified

--- library/cmake-build-debug/linker.sh
+++ library/cmake-build-debug/linker.sh
@@ -2,9 +2,11 @@ unlink ./conv_data
 unlink ./runfile
 
 if [ $1 = "0" ]; then
-    echo "runfile and conv_data unlinked"
+    echo "unlink ./conv_data"
+    echo "unlink ./runfile"
 else
     ln -s /Users/mari/work/MPPC/upgrade/cosmi/conv/cmake-build-debug/data ./conv_data
     ln -s /Users/mari/work/MPPC/upgrade/cosmi/rootfile ./runfile
-    echo "runfile and conv_data linked"
+    echo "ln -s /Users/mari/work/MPPC/upgrade/cosmi/conv/cmake-build-debug/data ./conv_data"
+    echo "ln -s /Users/mari/work/MPPC/upgrade/cosmi/rootfile ./runfile"
 fi
